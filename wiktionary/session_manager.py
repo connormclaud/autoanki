d6403f26c225b3e4d8026ed0c7451f8a30a96678
Vitali Vinahradski <connormclaud@gmail.com>
2024-11-02 00:24:02 +0100

    ruff: fix findings in session_manager.py

--- wiktionary/session_manager.py
+++ wiktionary/session_manager.py
@@ -4,16 +4,20 @@ import aiohttp
 
 
 class AiohttpSessionSingleton:
+    """Aiohttp Session Manager Singleton."""
+
     _session = None
 
     @classmethod
     async def get_session(cls) -> aiohttp.ClientSession:
+        """Get session or create if not exists."""
         if cls._session is None or cls._session.closed:
             cls._session = aiohttp.ClientSession()
         return cls._session
 
     @classmethod
     async def close_session(cls) -> None:
+        """Close open session."""
         if cls._session and not cls._session.closed:
             await cls._session.close()
             cls._session = None

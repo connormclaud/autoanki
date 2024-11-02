d92fe8c9133ad60b064aa7e4c9eacd25fee0e444
Vitali Vinahradski <connormclaud@gmail.com>
2024-11-02 01:25:04 +0100

    ruff: fix test_transcribe.py findings
    that was the last

--- tests/integration/test_transcribe.py
+++ tests/integration/test_transcribe.py
@@ -1,3 +1,5 @@
+"""Integration tests for transcribing sentences into IPA."""
+
 import pytest
 
 from wiktionary.ipa_service import WiktionaryIPA
@@ -7,11 +9,13 @@ from wiktionary.transcriber import IPATranscriber
 
 @pytest.mark.asyncio
 async def test_transcribe_sentence_integration() -> None:
-    """Integration test for transcribing a sentence using WiktionaryIPA and displaying the result."""
+    """Test transcribing a sentence using WiktionaryIPA."""
     # Example sentence transcription
     sentence = (
-        "nach der Sache mit dem Buchp dachte ich es wäre doch möglich dass auch durch das Lachen "
-        "manchmal mehr erreicht wird als durch das ständige Suchen nach einfachen Antworten doch"
+        "nach der Sache mit dem Buchp dachte ich es wäre doch"
+        " möglich dass auch durch das Lachen "
+        "manchmal mehr erreicht wird als durch das ständige "
+        "Suchen nach einfachen Antworten doch"
     )
 
     # Create instances for the test
@@ -29,6 +33,6 @@ async def test_transcribe_sentence_integration() -> None:
     assert len(transcription) > 0
     assert transcription == (
         r"naːx deːɐ̯ ˈzaxə mɪt deːm [Buchp] ˈdaxtə ɪç ɛs ˈvɛːʁə dɔx"
-        " ˈmøːklɪç das aʊ̯x dʊʁç das ˈlaxn̩ ˈmançmaːl meːɐ̯ ɛɐ̯ˈʁaɪ̯çt vɪʁt"
-        " als dʊʁç das ˈʃtɛndɪɡə ˈzuːxn̩ naːx ˈaɪ̯nfaxn̩ ˈantˌvɔʁtn̩ dɔx"
+        r" ˈmøːklɪç das aʊ̯x dʊʁç das ˈlaxn̩ ˈmançmaːl meːɐ̯ ɛɐ̯ˈʁaɪ̯çt vɪʁt"
+        r" als dʊʁç das ˈʃtɛndɪɡə ˈzuːxn̩ naːx ˈaɪ̯nfaxn̩ ˈantˌvɔʁtn̩ dɔx"
     )

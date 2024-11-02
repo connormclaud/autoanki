d78500d314b3c2052e41d66d3938487a05c5303e
Vitali Vinahradski <connormclaud@gmail.com>
2024-11-02 01:10:34 +0100

    ruff: fix findings in test_cli.py integration test
    and __init__.py in tests package

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,3 +1,6 @@
+"""Integration test for CLI."""
+
+from pathlib import Path
 from typer.testing import CliRunner
 
 from cli.cli import app
@@ -6,17 +9,21 @@ runner = CliRunner()
 
 
 def test_cli_parse_help() -> None:
-    """Test that the CLI parser returns the correct help message when '--help' is passed."""
+    """Test that the CLI parser returns the correct help message."""
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
     assert "Usage:" in result.stdout
 
 
 def test_cli_parse() -> None:
-    """Test that the CLI parser returns the correct help message when '--help' is passed."""
-    result = runner.invoke(app, ["de:Tisch\nen:Dish", "/tmp/1.txt"])
+    """Test that the CLI generate org file."""
+    import tempfile
+
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file_path = temp_file.name
+    result = runner.invoke(app, ["de:Tisch\nen:Dish", temp_file.name])
     assert result.exit_code == 0
-    with open("/tmp/1.txt") as file:
+    with Path(temp_file_path).open() as file:
         lines = file.readlines()
     assert any("en: Dish" in line for line in lines)
     assert any("de: Tisch" in line for line in lines)
